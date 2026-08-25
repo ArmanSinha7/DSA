b737f88f8df78e40a1ea064a61a804cbdf3bfae0
Arman Sinha <ar.mansinha07@gmail.com>
2026-08-25 16:07:10 +0530

    Optimize Missing Number solution

--- Missing_Number.cpp
+++ Missing_Number.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int sum=nums.size();
-        int asum=0;
         for(int i=0;i<nums.size();i++){
-            sum+=i;
-            asum+=nums[i];
+            sum+=i-nums[i];
         }
-        return sum-asum;
+        return sum;
     }
 };
