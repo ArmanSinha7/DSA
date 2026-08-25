ba99745b1eb2f6da30468358d9c8e983bb52f0ff
Arman Sinha <ar.mansinha07@gmail.com>
2026-08-25 16:03:25 +0530

    Add Missing Number solution

---
+++ Missing_Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum=nums.size();
+        int asum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=i;
+            asum+=nums[i];
+        }
+        return sum-asum;
+    }
+};
