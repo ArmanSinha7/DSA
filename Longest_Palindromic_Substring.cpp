e4c54758b0c5116d12834eee30fed555179f4957
Arman Sinha <ar.mansinha07@gmail.com>
2026-09-01 19:23:53 +0530

    Add Longest Palindromic Substring solution

---
+++ Longest_Palindromic_Substring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string t = "#";
+        for(char c:s){
+            t+=c;
+            t+="#";
+        }
+        int n = t.size();
+        vector<int> p(n,0);
+        int center = 0;
+        int right=0;
+        int maxlen=0;
+        int maxcentre=0;
+        for(int i=0;i<n;i++){
+            int mirror=2*center -i;
+            if(i<right){
+                p[i] = min(right-i,p[mirror]);
+            }
+            while(i+p[i]+1<n && i-p[i]-1>=0 && t[i+p[i]+1]==t[i-p[i]-1]){
+                p[i]++;
+            }
+            if(i+p[i]>right){
+                center=i;
+                right=i+p[i];
+            }
+            if(p[i]>maxlen){
+                maxlen=p[i];
+                maxcentre=i;
+            }
+        }
+        int start = (maxcentre-maxlen)/2;
+        return s.substr(start,maxlen);
+    }
+};
