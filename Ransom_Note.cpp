b657c68a34c6d01c2755b243cb280df933c8fbc2
Arman Sinha <ar.mansinha07@gmail.com>
2026-09-06 00:04:25 +0530

    Add Ransom Note solution

---
+++ Ransom_Note.cpp
@@ -0,0 +1,31 @@
+class Solution {
+
+public:
+
+    bool canConstruct(string ransomNote, string magazine) {
+
+        int arr[26]={0};
+
+        for(int i=0;i<magazine.size();i++){
+
+            arr[magazine[i]-'a']++;
+
+        }
+
+        for(int i=0;i<ransomNote.size();i++){
+
+            if(arr[ransomNote[i]-'a']==0){
+
+                return false;
+
+            }
+
+            arr[ransomNote[i]-'a']--;
+
+        }
+
+        return true;
+
+    }
+
+};
