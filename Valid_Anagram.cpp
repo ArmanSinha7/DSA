2a6f9d75b80d6159c1199dab23aebcb2ea910f53
Arman Sinha <ar.mansinha07@gmail.com>
2026-09-06 00:06:38 +0530

    Add Valid Anagram solution

---
+++ Valid_Anagram.cpp
@@ -0,0 +1,37 @@
+class Solution {
+
+public:
+
+    bool isAnagram(string s, string t) {
+
+        if(s.size()!=t.size()){
+
+            return false;
+
+        }
+
+        int arr[26]={0};
+
+        for(int i=0;i<s.size();i++){
+
+            arr[s[i]-'a']++;
+
+        }
+
+        for(int i=0;i<s.size();i++){
+
+            if(arr[t[i]-'a']==0){
+
+                return false;
+
+            }
+
+            arr[t[i]-'a']--;
+
+        }
+
+        return true;
+
+    }
+
+};
