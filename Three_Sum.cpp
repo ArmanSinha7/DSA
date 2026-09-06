7057e71f79e55e12710224ffd06837e1fb9049c9
Arman Sinha <ar.mansinha07@gmail.com>
2026-09-06 15:06:55 +0530

    Add Three Sum solution

---
+++ Three_Sum.cpp
@@ -0,0 +1,35 @@
+class Solution { 
+public: 
+    vector<vector<int>> threeSum(vector<int>& nums) { 
+        vector<vector<int>> ar; 
+        sort(nums.begin(), nums.end()); 
+        for(int i=0;i<nums.size();i++){ 
+            if(i>0 && nums[i]==nums[i-1]){ 
+                continue; 
+            } 
+            int left=i+1; 
+            int right=nums.size()-1; 
+            while(left<right){ 
+                if(nums[left]+nums[right]+nums[i]==0){ 
+                    ar.push_back({nums[left],nums[right],nums[i]}); 
+                    left++; 
+                    right--; 
+                    while(left<right && nums[left]==nums[left-1]){ 
+                        left++; 
+                    } 
+                    while(left<right && nums[right]==nums[right+1]){ 
+                        right--; 
+                    } 
+ 
+                } 
+                else if(nums[left]+nums[right]+nums[i]<0){ 
+                    left++; 
+                } 
+                else{ 
+                    right--; 
+                } 
+            } 
+        } 
+        return ar; 
+    } 
+};
