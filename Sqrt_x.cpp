86b7f989af546d44fabfaf05432620c06c278db1
Arman Sinha <ar.mansinha07@gmail.com>
2026-09-10 16:12:36 +0530

    Add LeetCode 69 Sqrt(x) solution

---
+++ Sqrt_x.cpp
@@ -0,0 +1,43 @@
+class Solution {
+
+public:
+
+    int mySqrt(int x) {
+
+        if(x<2){
+
+            return x;
+
+        }
+
+        long left=1;
+
+        long right = x;
+
+        long ans=1;
+
+        while(left<=right){
+
+            long mid = left+(right-left)/2;
+
+            if(mid*mid<=x){
+
+                left=mid+1;
+
+                ans = mid;
+
+            }
+
+            else{
+
+                right=mid-1;
+
+            }
+
+        }
+
+        return ans;
+
+    }
+
+};
