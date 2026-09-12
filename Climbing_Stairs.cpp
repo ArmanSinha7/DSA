6e6a3f135f6d9732f0228a1a9475c36f812b5739
Arman Sinha <ar.mansinha07@gmail.com>
2026-09-12 15:29:43 +0530

    Add Climbing Stairs solution

---
+++ Climbing_Stairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+
+public:
+
+    int climbStairs(int n) {
+
+        int n1=1;
+
+        int n2=1;
+
+        for(int i=1;i<n;i++){
+
+            int temp= n2;
+
+            n2= n1+n2;
+
+            n1=temp;
+
+        }
+
+        return n2;
+
+    }
+
+};
